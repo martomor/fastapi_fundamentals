cc0d30798e6f85353ecf87d22a61e85e423b0943
martomor <martinmoreno313@gmail.com>
2022-10-08 13:00:42 -0500

    loading from json files

--- schemas.py
+++ schemas.py
@@ -1,3 +1,4 @@
+import json
 from pydantic import BaseModel
 
 class Car(BaseModel): #Pydantic makes sure to parse all items into the types specified
@@ -8,3 +9,8 @@ class Car(BaseModel): #Pydantic makes sure to parse all items into the types spe
     transmission: str|None = "auto"
 # pydantic methods can be accesed as:
 # c.json() or c.dict()
+
+def load_db() -> list[Car]:
+    """Load a list of Car objects from a JSON file"""
+    with open ("cars.json") as f:
+        return [Car.parse_obj(obj) for obj in json.load(f)]
