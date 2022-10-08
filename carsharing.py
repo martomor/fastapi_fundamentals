bbfa5b93d0f0b7951f2369b4cb0df0d0f4fa11d2
martomor <martinmoreno313@gmail.com>
2022-10-08 11:21:20 -0500

    adding a parameter

--- carsharing.py
+++ carsharing.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def welcome():
+def welcome(name):
     """Return a friendly welcome message"""
-    return {'message': "Welcome to the car sharing service"}
+    return {'message': f"Welcome {name}, to the car sharing service"}
+
+#You can make a request like this: http://127.0.0.1:8000/?name=Martin
