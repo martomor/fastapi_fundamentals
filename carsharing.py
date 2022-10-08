0abddd3b0febc26807460988ff4dee3fd576d72b
martomor <martinmoreno313@gmail.com>
2022-10-08 11:32:47 -0500

    get operation to return all cars from a list

--- carsharing.py
+++ carsharing.py
@@ -2,9 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+db = [
+     {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+     {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+     {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+     {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+     {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+     {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+     {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+     {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+     {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+ ]
+
 @app.get("/")
 def welcome(name):
     """Return a friendly welcome message"""
     return {'message': f"Welcome {name}, to the car sharing service"}
 
 #You can make a request like this: http://127.0.0.1:8000/?name=Martin
+
+@app.get("/api/cars")
+def get_cars():
+    """Return all cars"""
+    return db
