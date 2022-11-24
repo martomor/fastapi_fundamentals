ac779c4d4de47fddffd75a612f87ac82ca9816ce
martomor <martinmoreno313@gmail.com>
2022-11-23 20:54:38 -0500

    processing Form data

--- routers/web.py
+++ routers/web.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form, Depends
+from sqlmodel import Session
 from starlette.responses import HTMLResponse #Library used to serve http
 from fastapi.templating import Jinja2Templates #For serving HTML files
+
+from db import get_session
+from routers.cars import get_cars
 
 router = APIRouter()
 
@@ -10,3 +14,11 @@ templates = Jinja2Templates(directory="templates")
 def home(request: Request):
     return templates.TemplateResponse("home.html", #Will read the home.html file and return the proper response
                                      {"request": request}) #This is mandatory
+
+@router.post("/search", response_class=HTMLResponse)
+def search(*, size: str = Form(...), doors: int = Form(...), #after the * everything becomes a key argument
+           request: Request,
+           session: Session = Depends(get_session)):
+    cars = get_cars(size=size, doors=doors, session=session)
+    return templates.TemplateResponse("search_results.html",
+                                     {"request": request, "cars":cars})
