22f5ed10f02d861ca95f3631f996eda42eb71fd1
martomor <martinmoreno313@gmail.com>
2022-11-23 19:49:23 -0500

    dynamic html with Jinja

--- routers/web.py
+++ routers/web.py
@@ -1,18 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.responses import HTMLResponse #Library used to serve http
+from fastapi.templating import Jinja2Templates #For serving HTML files
 
 router = APIRouter()
 
+templates = Jinja2Templates(directory="templates")
+
 @router.get("/", response_class=HTMLResponse) # so that we dont response json
-def home():
-    return """
-    <html>
-        <head>
-            <title>Carsharing Demo</title>
-        </head>
-        <body>
-            <h1>Welcome to the Car Sharing service</h1>
-            <p>Here is some text for you </p>
-        </body>
-    </html>
-    """
+def home(request: Request):
+    return templates.TemplateResponse("home.html", #Will read the home.html file and return the proper response
+                                     {"request": request}) #This is mandatory
