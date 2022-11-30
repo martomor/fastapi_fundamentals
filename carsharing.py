84fc86b6857943022a540924504b6cd299ffbb76
martomor <martinmoreno313@gmail.com>
2022-11-30 18:15:59 -0500

    status codes and error handling

--- carsharing.py
+++ carsharing.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
 from sqlmodel import SQLModel
 from routers import cars, web
+from routers.cars import BadTripException
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from db import engine
 
@@ -13,6 +16,13 @@ app.include_router(web.router)
 @app.on_event("startup") # On startup will be loaded after all the code has been declared
 def on_startup():
     SQLModel.metadata.create_all(engine) #Creates the database at start event
+
+@app.exception_handler(BadTripException) # Status Codes and Error Handling
+async def unicorn_exception_handler(request: Request, exc:BadTripException):
+    return JSONResponse(
+        status_code = HTTP_422_UNPROCESSABLE_ENTITY,
+        current = {"message" : "Bad Trip"},
+    )
     
 if __name__ == "__main__":
     uvicorn.run("carsharing:app", reload=True)
