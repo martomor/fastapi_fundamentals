3984756aaa58cd0e7aa1ddbcd5cd2c6ac133ebcc
martomor <martinmoreno313@gmail.com>
2022-11-30 18:56:13 -0500

    CORS Middleware

--- carsharing.py
+++ carsharing.py
@@ -5,6 +5,7 @@ from routers import cars, web
 from routers.cars import BadTripException
 from starlette.responses import JSONResponse
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import engine
 
@@ -12,6 +13,19 @@ from db import engine
 app = FastAPI(title="Car Sharing")
 app.include_router(cars.router)
 app.include_router(web.router)
+
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8080",
+]
+
+app.add_middleware( # To enable another domains to ping our api
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True, 
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
 @app.on_event("startup") # On startup will be loaded after all the code has been declared
 def on_startup():
