f9b7f01c394c390c028888fd8d13a18138793361
martomor <martinmoreno313@gmail.com>
2022-11-30 18:32:36 -0500

    middleware

--- carsharing.py
+++ carsharing.py
@@ -23,6 +23,12 @@ async def unicorn_exception_handler(request: Request, exc:BadTripException):
         status_code = HTTP_422_UNPROCESSABLE_ENTITY,
         current = {"message" : "Bad Trip"},
     )
+
+@app.middleware("http")
+async def add_cars_cookie(request: Request, call_next):
+    response = await call_next(request)
+    response.set_cookie(key="cars_cookie", value = "you_visited_the_carsharing_app")
+    return response
     
 if __name__ == "__main__":
     uvicorn.run("carsharing:app", reload=True)
